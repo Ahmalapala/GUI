3ddc0530c74063ec9934321e0e11346ffc15fd41
Ahmalapala <ahmalchambers27@gmail.com>
2022-12-01 00:11:13 -0500

    Implemented JFileChooser

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -4,43 +4,56 @@ import javax.swing.JFrame;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Desktop;
 import java.io.File;
 
-public class MyFrame extends JFrame implements ActionListener{
+public class MyFrame implements ActionListener{
     JButton btn;
-    public static void main() {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Specify a file to save");
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(new FlowLayout());
+
+        JButton btn = new JButton("Upload file");
+        btn.setFocusable(false);
+        btn.addActionListener (new ActionListener(){
+            public void actionPerformed(ActionEvent evt) {
+                if(evt.getSource()==btn){
+        
+                    JFileChooser file_upload = new JFileChooser();
+                    //file_upload.showOpenDialog(null);
+                    //int response = file_upload.showOpenDialog(null);
+                    int response_2 = file_upload.showSaveDialog(null);
+        
+                    if(response_2 == JFileChooser.APPROVE_OPTION){
+                        File file_path = new File(file_upload.getSelectedFile().getAbsolutePath());
+                        System.out.println(file_path);
+                        try {
+                            Desktop.getDesktop().open(file_path);
+                          } catch (Exception e) {
+                            e.printStackTrace();
+                          }
+                    }
+                }
+            }
+        });
+
+        frame.add(btn);
+
+        frame.pack();
+        frame.setVisible(true);    
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
         
     }
-    MyFrame(){
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Demo");
-        this.setLocationRelativeTo(null);
-        this.setLayout(new FlowLayout());
+    /*MyFrame(){
 
-        btn = new JButton("Upload file");
-        btn.setFocusable(false);
-        btn.addActionListener(this);
+    }*/
 
-        this.add(btn);
 
-        this.pack();
-        this.setVisible(true);
 
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent evt) {
-        if(evt.getSource()==btn){
-
-            JFileChooser file_upload = new JFileChooser();
-            //file_upload.showOpenDialog(null);
-            //int response = file_upload.showOpenDialog(null);
-            int response_2 = file_upload.showSaveDialog(null);
-
-            if(response_2 == JFileChooser.APPROVE_OPTION){
-                File file_path = new File(file_upload.getSelectedFile().getAbsolutePath());
-                System.out.println(file_path);
-            }
-        }
-    }
 }
